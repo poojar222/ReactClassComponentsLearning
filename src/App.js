af2e0dde7123a3dae96a2f2def5d79b9a39eedbc
pooja <pooja.raghu@zohocorp.com>
2024-11-11 10:41:54 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from "react";
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Card extends React.Component{
+
+  constructor(props){
+    super(props);
+      this.state = {
+        follow : "Follow",
+        followerscount : 5 ,
+        followingcount : 1
+      }
+  }
+
+  ChangeFollowState = () =>{
+    this.state.follow === "Follow" ? this.setState({follow: "Following", followerscount:  this.state.followerscount+1}) : 
+                                    this.setState({follow: "Follow", followerscount: this.state.followerscount-1}) ; 
+  }
+
+  render(){
+    return <>
+           
+           <div className="profileCard">
+            <img src ={this.props.path} alt="Image"/>
+            <div className="profileTexts">
+              <div className="userNameButton">
+                <h3>{this.props.name}</h3>
+                <button onClick={this.ChangeFollowState}>{this.state.follow}</button>
+              </div>
+              <div className="followsText">
+                <h3> Posts </h3>
+                <h3> {this.state.followerscount}Followers </h3>
+                <h3>{this.state.followingcount} Following </h3>
+              </div>
+              <h1>name</h1>
+              <h3>bio</h3>
+              </div>
+              </div>
+          </>
+  }
+
+
+
+
 }
 
-export default App;
+export default Card;
